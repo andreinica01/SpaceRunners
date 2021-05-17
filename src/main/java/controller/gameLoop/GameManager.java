f9452f02d846bc73044d2f402c7f3212ab0537e7
andrewasd <andrei.nica@studio.unibo.it>
2021-05-17 16:01:10 +0200

    Gestione del frame/sec

--- src/main/java/controller/gameLoop/GameManager.java
+++ src/main/java/controller/gameLoop/GameManager.java
@@ -1,7 +1,5 @@
 package controller.gameLoop;
 
-
-
 import controller.gameController.GameContollerImpl;
 import controller.gameController.GameController;
 import javafx.animation.*;
@@ -14,45 +12,57 @@ import Utilities.Parameters;
 
 public class GameManager extends AnimationTimer {
 
+    long delta;
+    long prevtime;
+    private long sleepNs = 10000000;
+
     private MainWindow mainwindow;
 
     private GameFieldImpl gamefield;
 
     private GameController gameController;
 
-
-    public GameManager(Stage stage)
-    {
+    public GameManager(Stage stage) {
 
         this.mainwindow = new MainWindowImpl(stage);
-        this.gamefield = new GameFieldImpl(Parameters.WIDTH,Parameters.HEIGHT);
-        
+        this.gamefield = new GameFieldImpl(Parameters.WIDTH, Parameters.HEIGHT);
+
         this.mainwindow.setWidth(Parameters.WIDTH);
         this.mainwindow.setHeight(Parameters.HEIGHT);
-    
+
         this.mainwindow.addGameField(this.gamefield);
         this.gameController = new GameContollerImpl(this.gamefield);
-         
-        
+
         this.mainwindow.setVisible(true);
-     
 
     }
 
- 
-
+    public double getFrameRateHertz(long delta) {
+        double frameRate = 1d / delta;
+        return frameRate * 1e9;
+    }
 
     @Override
-    public void handle(long arg0) {
-        
-        
-        this.gameController.update();
+    public void handle(long now) {
+
+        /* if ((now - prevtime) < sleepNs) {
+            System.out.println("exit");
+            return;
+        } */
+
+        try {
+            Thread.sleep(0,100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
        
+            this.gameController.update();
+            
+          
+            prevtime = now;
 
-        
-        
-        
+
     }
-    
+
 }
