75ae97bfd9cba9b940eba32ce7a29ce13819261c
andrewasd <andrei.nica@studio.unibo.it>
2021-05-17 16:00:40 +0200

    gestione navi nemiche

--- src/main/java/controller/gameController/GameContollerImpl.java
+++ src/main/java/controller/gameController/GameContollerImpl.java
@@ -3,11 +3,15 @@ package controller.gameController;
 import Utilities.Direction;
 import Utilities.InputCommand;
 import Utilities.Vector2DImpl;
+import controller.enemyAI.enemyAI;
 import controller.frameManager.FrameManager;
 import controller.inputController.InputControllerImpl;
 import model.bullet.Bullet;
 import model.ship.SpaceShip;
+import view.SoundManager.SoundManager;
 import view.gameField.GameField;
+
+
 import Utilities.*;
 
 public class GameContollerImpl implements GameController {
@@ -15,42 +19,50 @@ public class GameContollerImpl implements GameController {
     private GameField gamefield;
     private FrameManager frame;
     InputCommand userCommand;
+    Parameters param;
     // private Set<Entity> enemies;
 
     private SpaceShip player;
     private InputControllerImpl inputController;
+    private enemyAI AIController;
 
     public GameContollerImpl(GameField gamefield) {
 
         this.gamefield = gamefield;
         this.inputController = new InputControllerImpl();
+
         this.player = new SpaceShip();
 
-        /* setup player info */
+        param = new Parameters();
         this.player.setDimension(new Vector2DImpl<Number>(100, 100));
-        this.player.setImage(Parameters.ImageFolder + "spaceship2.png");
+        this.player.setImage(Parameters.playerImage);
         this.player.setSpeed(4);
         this.player.setDirection(Direction.NONE);
         this.player.setPosition(this.gamefield.getWidth().intValue() / 2, this.gamefield.getHeight().intValue() - 200);
+        /* setup player info */
 
         this.gamefield.setPlayer(this.player);
 
         this.frame = new FrameManager(this.gamefield);
         this.gamefield.setInputController(this.inputController);
 
-        this.gamefield.setBackgroundImage(Parameters.ImageFolder+ "stars.png");
+        this.gamefield.setBackgroundImage(Parameters.ImageFolder + "back.png");
+
+        this.AIController = new enemyAI(this.gamefield);
 
     }
 
     private void playerAttack() {
 
         Bullet x = new Bullet().bulletDamage(10);
-       
-        x.setPosition(this.player.getNode().getTranslateX()-190, this.player.getNode().getTranslateY()-200);
+
+        x.setPosition(this.player.getNode().getTranslateX() - 190, this.player.getNode().getTranslateY() - 200);
 
         x.setSpeed(10);
 
         this.gamefield.addBullet(x);
+
+        SoundManager.playBulletSound();
 
     }
 
@@ -58,13 +70,17 @@ public class GameContollerImpl implements GameController {
 
         userCommand = this.inputController.getCommand();
 
-        if (userCommand == InputCommand.GO_LEFT)
+        if (userCommand == InputCommand.GO_LEFT) {
             this.player.setDirection(Direction.LEFT);
-        else if (userCommand == InputCommand.GO_RIGHT)
-            this.player.setDirection(Direction.RIGHT);
-        else if (userCommand == InputCommand.ATTACK)
-            playerAttack();
 
+        } else if (userCommand == InputCommand.GO_RIGHT) {
+            this.player.setDirection(Direction.RIGHT);
+
+        } else if (userCommand == InputCommand.ATTACK) {
+            playerAttack();
+        }
+
+        this.AIController.update();
         frame.update();
 
     }
