0e7ffe751b3e38a1e082488fa3e93a82aeadcfb7
andrewasd <andrei.nica@studio.unibo.it>
2021-05-17 16:02:05 +0200

    Gestione Suoni

---
+++ src/main/java/view/SoundManager/SoundManager.java
@@ -0,0 +1,34 @@
+package view.SoundManager;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import Utilities.Parameters;
+
+public class SoundManager {
+
+    static private void playSound(String sound) {
+        try {
+
+            Clip bulletSound = AudioSystem.getClip();
+            bulletSound.open(AudioSystem.getAudioInputStream(new File(Parameters.SoundFolder + sound)));
+
+            bulletSound.start();
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    static public void playBulletSound() {
+        playSound("laser.wav");
+    }
+
+    static public void playShipPassing() {
+        playSound("passingby.wav");
+    }
+
+}
