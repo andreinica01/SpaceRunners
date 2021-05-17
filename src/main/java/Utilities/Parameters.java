5e2c823fa1ee8761e1bf6096665ef257a9b0eae8
andrewasd <andrei.nica@studio.unibo.it>
2021-05-17 16:03:44 +0200

    Parametri del gioco

--- src/main/java/Utilities/Parameters.java
+++ src/main/java/Utilities/Parameters.java
@@ -2,17 +2,40 @@ package Utilities;
 
 import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
 import javafx.scene.image.Image;
 
-public interface Parameters {
+public class Parameters {
 
-    final int WIDTH = 500;
-    final int HEIGHT = 720;
-
-    static String ImageFolder = "src/main/resources/Images/";
-    static Image bulletImage = new Image(new File(ImageFolder+"bullet.png").toURI().toString());
-    static Image BackgroundImage = new Image(new File(ImageFolder+"background.jpg").toURI().toString());
+    public Parameters()
+    {
+        try {
+            bulletSound = AudioSystem.getAudioInputStream(new File(Parameters.SoundFolder+"laser.wav"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      
+    }
     
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 720;
 
+    public static final int INITIAL_PLAYER_POINTS = 10;
+
+    public static String ResourcesFolder = "src/main/resources/";
+    public static String ImageFolder = ResourcesFolder+"Images/";
+    public static String SoundFolder = ResourcesFolder+"Sounds/";
+
+    /*Images*/
+    public static Image bulletImage = new Image(new File(ImageFolder+"bullet.png").toURI().toString());
+    public static Image BackgroundImage = new Image(new File(ImageFolder+"stars2.png").toURI().toString());
+    public static Image playerImage = new Image(new File(ImageFolder+"spaceship2.png").toURI().toString());
+    public static Image enemyImage = new Image(new File(ImageFolder+"enemyship.png").toURI().toString());
+
+    /*Sounds */
+    public static AudioInputStream bulletSound ;
+  
 
 }
