4d9b5b941ed9eebb7f264f12b6935d4068abdd96
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2021-08-05 17:53:02 +0200

    Adding Status Factory

--- src/main/java/model/status/StatusFactory.java
+++ src/main/java/model/status/StatusFactory.java
@@ -7,22 +7,23 @@ import model.status.malus.MalusFire;
 import model.status.malus.MalusSpeed;
 
 public class StatusFactory {
-	
-    public StatusFactory(){}
-    
-    public Status createStatus (StatusEnum type){
-        switch (type){
-            case BonusLife:
-                return new BonusLife();
-            case BonusSpeed:
-                return new BonusSpeed();
-            case MalusCommand:
-            	return new MalusCommand();
-            case MalusFire:
-            	return new MalusFire();   
-            case MalusSpeed:
-            	return new MalusSpeed(); 	
-        }
+
+	public StatusFactory() {
+	}
+
+	public Status createStatus(StatusEnum type) {
+		switch (type) {
+		case BonusLife:
+			return new BonusLife();
+		case BonusSpeed:
+			return new BonusSpeed();
+		case MalusCommand:
+			return new MalusCommand();
+		case MalusFire:
+			return new MalusFire();
+		case MalusSpeed:
+			return new MalusSpeed();
+		}
 		return null;
-    }
+	}
 }
