ecacef379d90864232470d73d18677817043314d
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-08-17 18:05:19 +0200

    trying new solutions to hud problem

---
+++ src/main/java/gameEventController/GameEventController.java
@@ -0,0 +1,13 @@
+package gameEventController;
+
+import controller.hudcontroller.*;
+import controller.collisionEngine.*;
+
+public class GameEventController {
+    
+    //che cazzo ci devo passare?
+    public GameEventController() {
+        
+    }
+    
+}
