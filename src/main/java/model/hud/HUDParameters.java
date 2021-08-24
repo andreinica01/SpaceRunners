18f9dbba9b8f65c54bd099a414e6fba547d64abe
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-08-24 16:35:22 +0200

    added all parameters

--- src/main/java/model/hud/HUDParameters.java
+++ src/main/java/model/hud/HUDParameters.java
@@ -10,6 +10,9 @@ public class HUDParameters {
 
     public final static int ZERO = 0;
     public final static int ONE = 1;
+    public final static int TWO = 2;
+    public final static int THREE = 3;
+    public final static int FOUR = 4;
     public final static int FIVE = 5;
     
     public final static String PNG_FOLDER = Parameters.ImageFolder;
