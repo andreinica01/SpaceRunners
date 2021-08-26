fdfd1f512bbb34a38a8522aa92277cdf0db169b4
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-08-26 17:02:19 +0200

    fixing indentation and javadoc

--- src/main/java/view/gameField/GameField.java
+++ src/main/java/view/gameField/GameField.java
@@ -13,58 +13,86 @@ import model.status.Status;
 import view.SoundManager.SoundManager;
 
 public interface GameField {
-  /** @return the draw panel where the content is draw */
-  AnchorPane getGameContainer();
+    /** @return the draw panel where the content is drawn. */
+    AnchorPane getGameContainer();
 
-  /** @return the active entities on the Gamefield */
-  Set<Entity> getActiveEntities();
+    /** @return the active entities on the Gamefield. */
+    Set<Entity> getActiveEntities();
 
-  /** @return Width */
-  Number getWidth();
+    /** @return Width. */
+    Number getWidth();
 
-  /*
-   * @return Height
-   */
-  Number getHeight();
+    /** @return Height. */
+    Number getHeight();
 
-  /** @param add the player Entity to the game */
-  void setPlayer(SpaceShip player);
+    /** @param add the player Entity to the game. */
+    void setPlayer(SpaceShip player);
 
-  /** @return the player Entity */
-  SpaceShip getPlayer();
+    /** @return the player Entity. */
+    SpaceShip getPlayer();
 
-  /** @return a set with all the current enemy ships on the field */
-  Set<SpaceShip> getActiveEnemyShips();
+    /** @return a set with all the current enemy ships on the field. */
+    Set<SpaceShip> getActiveEnemyShips();
 
-  /** @return a set with all the Bonus Objects in the game */
-  Set<Status> getBonusObjects();
+    /** @return a set with all the Bonus Objects in the game. */
+    Set<Status> getBonusObjects();
 
-  /** @return all active bullets shot by the player */
-  Set<Bullet> getActiveBulletsShotbyPlayer();
+    /** @return all active bullets shot by the player. */
+    Set<Bullet> getActiveBulletsShotbyPlayer();
 
-  /** @return all active bullets shot towards the player */
-  Set<Bullet> getActiveBulletsShotbyEnemies();
+    /** @return all active bullets shot towards the player. */
+    Set<Bullet> getActiveBulletsShotbyEnemies();
 
-  /** @return the scene. */
-  Scene getScene();
+    /** @return the scene. */
+    Scene getScene();
 
-  void setInputController(InputControllerImpl controller);
+    /**
+     * Sets the input controller.
+     * @param controller
+     */
+    void setInputController(InputControllerImpl controller);
 
-  SoundManager getSoundManager();
+    /** @return the sound manager class. */
+    SoundManager getSoundManager();
 
-  void setScene(Scene scene);
+    /**
+     * Set the scene.
+     * @param scene to be set.
+     */
+    void setScene(Scene scene);
 
-  void setBackgroundImage(String path);
+    /** Set the background image. 
+     * @param the path of the file.
+     */
+    void setBackgroundImage(String path);
 
-  Node[] getBackground();
+    /** @return the background. */
+    Node[] getBackground();
 
-  void addBullet(Bullet bullet);
+    /**
+     * Add a bullet to the game.
+     * @param bullet.
+     */
+    void addBullet(Bullet bullet);
 
-  void addEnemyShip(SpaceShip enemy);
+    /**
+     * Add an enemy ship to the game.
+     * @param enemy.
+     */
+    void addEnemyShip(SpaceShip enemy);
 
-  void addBonus(Status bonus);
+    /**
+     * Add a bonus to the game.
+     * @param bonus
+     */
+    void addBonus(Status bonus);
 
-  StatusController getStatusController();
+    /** @return StatusController */
+    StatusController getStatusController();
 
-  void setStatusController(StatusController statusController);
+    /**
+     * Set the status controller.
+     * @param statusController to be set.
+     */
+    void setStatusController(StatusController statusController);
 }
