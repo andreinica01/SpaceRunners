c0682ea3308a7c60a92bdb2510a28e2e1fcc688c
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-08-26 20:04:02 +0200

    fixed identation ad javadoc

--- src/main/java/model/status/StatusEnum.java
+++ src/main/java/model/status/StatusEnum.java
@@ -2,16 +2,20 @@ package model.status;
 
 import java.util.Random;
 
-/** Enum containing all Status type. */
+/**
+ * Enum containing all Status type.
+ */
 public enum StatusEnum {
-  BonusLife,
-  BonusSpeed,
-  MalusCommand,
-  MalusFire,
-  MalusSpeed;
+    BonusLife, 
+    BonusSpeed, 
+    MalusCommand, 
+    MalusFire, 
+    MalusSpeed;
 
-  /** Return a random Status type. */
-  public static StatusEnum getRandom() {
-    return StatusEnum.values()[new Random().nextInt(StatusEnum.values().length)];
-  }
+    /**
+     * Return a random Status type.
+     */
+    public static StatusEnum getRandom() {
+        return StatusEnum.values()[new Random().nextInt(StatusEnum.values().length)];
+    }
 }
