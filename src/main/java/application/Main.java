801f75eeb7c83023c871c6261fa2e272889ecdca
SalvatoreBennici <Salvatore.bennici@studio.unibo.it>
2021-09-20 18:05:37 +0200

    Adding exit on close default operation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,13 +2,14 @@ package application;
 
 import controller.gameSwitcher.SwitcherImpl;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public final void start(final Stage mainWindow) {
-
+    	mainWindow.setOnCloseRequest(e -> Platform.exit());
     	new SwitcherImpl(mainWindow);
         mainWindow.setTitle("SpaceRunners");
     }
