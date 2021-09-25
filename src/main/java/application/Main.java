a36d2373d2a738a7efcaded8681b5b04fd22ee32
SalvatoreBennici <salvatorebennici@studio.unibo.it>
2021-09-25 14:41:40 +0200

    Updating menu

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,28 +2,38 @@ package application;
 
 import java.io.IOException;
 
+import controller.gameSwitcher.MenuController;
 import controller.gameSwitcher.SceneManager;
 
 
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
-	public final void start(final Stage mainWindow) throws IOException {
+	public final void start(Stage mainWindow) throws IOException {
 		mainWindow.setTitle("SpaceRunners");
+		
 		mainWindow.setOnCloseRequest(e -> {
 			Platform.exit();
 			System.exit(0);
 		});
-		SceneManager switcher = new SceneManager(mainWindow);
-		switcher.switchToStartMenu();
+		this.createStartMenu(mainWindow);
+		mainWindow.show();
 	}
 
 	public static void main(final String[] args) {
 		launch(args);
+	}
+	
+	private void createStartMenu (Stage mainWindow) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/StartMenu.fxml"));
+		loader.setController(new MenuController(new SceneManager(mainWindow)));
+		mainWindow.setScene(new Scene(loader.load()));
 	}
 }
--- src/main/java/controller/gameSwitcher/SceneManager.java
+++ src/main/java/controller/gameSwitcher/SceneManager.java
@@ -3,28 +3,33 @@ package controller.gameSwitcher;
 import java.io.IOException;
 
 import controller.gameLoop.GameManager;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class SceneManager {
 
 	private Stage mainWindow;
 	private GameManager gameManager;
-	
 
 	public SceneManager(final Stage mainWindow) throws IOException {
 		this.mainWindow = mainWindow;
-		this.gameManager = new GameManager(mainWindow);
-	}
-
-	public void switchToStartMenu() throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/StartMenu.fxml"));
-		loader.setController(new MenuController(this.mainWindow));
-		this.mainWindow.setScene(new Scene(loader.load()));
 	}
 
 	public void switchToGame() throws IOException {
+		this.gameManager = new GameManager(this.mainWindow);
 		this.gameManager.start();
+		System.out.println("Game Size");
+		System.out.println(this.mainWindow.getScene().getHeight());
+		System.out.println(this.mainWindow.getScene().getWidth());
+
+	}
+	
+	public void switchToScores() {
+		System.out.println("scores");
+	}
+	
+	public void quit() {
+		Platform.exit();
+		System.exit(0);
 	}
 }
