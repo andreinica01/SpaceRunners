f461008689e08ff31132884cc99025d59ba4c2ae
SalvatoreBennici <salvatorebennici@studio.unibo.it>
2021-09-25 19:27:03 +0200

    merge

--- src/main/java/controller/gameSwitcher/MenuController.java
+++ src/main/java/controller/gameSwitcher/MenuController.java
@@ -3,50 +3,37 @@ package controller.gameSwitcher;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-<<<<<<< HEAD
 import javafx.scene.control.TextField;
-=======
 import view.SoundManager.SoundManager;
->>>>>>> ac92e73154a68f56995ab1e45ff280c8c43f320d
 
 public class MenuController {
 
 	private SceneManager sceneManager;
-<<<<<<< HEAD
-	
-	@FXML 
-	private TextField nickname;
-
-	public MenuController(SceneManager sceneManager) {
-=======
 	private SoundManager soundManager;
+
+	@FXML
+	private TextField nickname;
 
 	/*
 	 * Constructor.
 	 */
 	public MenuController(final SceneManager sceneManager) {
->>>>>>> ac92e73154a68f56995ab1e45ff280c8c43f320d
 		this.sceneManager = sceneManager;
 		this.soundManager = new SoundManager();
 		this.soundManager.playMusicMenu();
 	}
 
 	@FXML
-	private void quit(final ActionEvent event) {
+	void quit(final ActionEvent event) {
 		this.sceneManager.quit();
 	}
 
 	@FXML
-<<<<<<< HEAD
-	void showScores(ActionEvent event) throws IOException {
-=======
-	private void showScores(final ActionEvent event) {
->>>>>>> ac92e73154a68f56995ab1e45ff280c8c43f320d
+	void showScores(final ActionEvent event) throws IOException {
 		this.sceneManager.switchToScores();
 	}
 
 	@FXML
-<<<<<<< HEAD
 	void pickNickname(ActionEvent event) throws IOException {
 		this.sceneManager.switchToNickname();
 	}
@@ -55,16 +42,17 @@ public class MenuController {
 	void showStartMenu(ActionEvent event) throws IOException {
 		this.sceneManager.switchToStartMenu();
 	}
-    
+
 	@FXML
 	void nicknameEvent(ActionEvent event) throws IOException {
-	String name = this.nickname.getText();
-	if (!name.isBlank()) {
-		this.sceneManager.switchToGame(name);
+		String name = this.nickname.getText();
+		if (!name.isBlank()) {
+			this.sceneManager.switchToGame(name);
 		}
-=======
-	private void startGame(final ActionEvent event) throws IOException {
-		this.sceneManager.switchToGame();
->>>>>>> ac92e73154a68f56995ab1e45ff280c8c43f320d
+	}
+	
+	@FXML
+	void startGame(final ActionEvent event) throws IOException {
+		this.sceneManager.switchToNickname();
 	}
 }
