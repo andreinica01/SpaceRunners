68978f24248ebf66bdb02b3619763bb49ee6ed77
SalvatoreBennici <salvatorebennici@studio.unibo.it>
2021-10-07 23:45:52 +0200

    Fixing initial stage size

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,15 +1,19 @@
 package application;
 
 import java.io.IOException;
+
 import controller.gameSwitcher.SceneManager;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
+import utilities.MagicEnumInt;
 
 public class Main extends Application {
 
 	@Override
 	public final void start(final Stage mainWindow) throws IOException {
+		mainWindow.setHeight(Double.valueOf(MagicEnumInt.HEIGHT.getValue()));
+		mainWindow.setWidth(Double.valueOf(MagicEnumInt.WIDTH.getValue()));
 		mainWindow.setTitle("SpaceRunners");
 		mainWindow.setResizable(false);
 		mainWindow.setOnCloseRequest(e -> {
