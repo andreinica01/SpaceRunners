1cf9189c7c78a7a3ce5fe25cdf2ec010c5e1a9fd
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-10-07 22:53:32 +0200

    new magic number implementation

---
+++ src/main/java/utilities/MagicEnumComplexImage.java
@@ -0,0 +1,61 @@
+package utilities;
+
+import java.io.File;
+
+import javafx.scene.image.Image;
+
+public enum MagicEnumComplexImage {
+
+	/**
+	 * 
+	 */
+	BONUS_LIFE(new Image(new File(MagicEnumString.BONUS_LIFE_PATH.getValue()).toURI().toString(), 
+			50, 50, false, true)),
+
+	/**
+	 * 
+	 */
+	BONUS_SPEED(new Image(new File(MagicEnumString.BONUS_SPEED_PATH.getValue()).toURI().toString(), 
+			50, 50, true, true)),
+
+	/**
+	 * 
+	 */
+	MALUS_COMMAND(new Image(new File(MagicEnumString.MALUS_COMMAND_PATH.getValue()).toURI().toString(), 
+			50, 50, true, true)),
+
+	/**
+	 * 
+	 */
+	MALUS_FIRE(new Image(new File(MagicEnumString.MALUS_FIRE_PATH.getValue()).toURI().toString(), 
+			50, 50, true, true)),
+
+	/**
+	 * 
+	 */
+	MALUS_SPEED(new Image(new File(MagicEnumString.MALUS_SPEED_PATH.getValue()).toURI().toString(), 
+			50, 50, true, true)),
+
+	/**
+	 * 
+	 */
+	RANDOM_STATUS(new Image(new File(MagicEnumString.RANDOM_STATUS_PATH.getValue()).toURI().toString(), 
+			50, 50, true, true)),
+	
+	private Image value;
+
+	/**
+	 * Constructor.
+	 * @param value
+	 */
+	private MagicEnumComplexImage(final Image value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return enum value.
+	 */
+	public Image getImage() {
+		return this.value;
+	}
+}
---
+++ src/main/java/utilities/MagicEnumImage.java
@@ -0,0 +1,54 @@
+package utilities;
+
+import java.io.File;
+import javafx.scene.image.Image;
+
+public enum MagicEnumImage {
+
+	/**
+	 * 
+	 */
+	BULLET(new Image(new File(MagicEnumString.BULLET_PATH.getValue()).toURI().toString()),
+
+	/**
+	 * 
+	 */
+	BACKGROUND(new Image(new File(MagicEnumString.BACKGROUND_PATH.getValue()).toURI().toString()),
+
+	/**
+	 * 
+	 */
+	PLAYER(new Image(new File(MagicEnumString.PLAYER_PATH.getValue()).toURI().toString()),
+
+	/**
+	 * 
+	 */
+	ENEMY(new Image(new File(MagicEnumString.ENEMY_PATH.getValue()).toURI().toString()),
+
+	/**
+	 * 
+	 */
+	LIFE(new Image(new File(MagicEnumString.LIFE_PATH.getValue()).toURI().toString()),
+
+	/**
+	 * 
+	 */
+	BOSS(new Image(new File(MagicEnumString.BOSS_PATH.getValue()).toURI().toString());
+	
+	private Image value;
+
+	/**
+	 * Constructor.
+	 * @param value
+	 */
+	private MagicEnumImage(final Image value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return enum value.
+	 */
+	public Image getImage() {
+		return this.value;
+	}
+}
