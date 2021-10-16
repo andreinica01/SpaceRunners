abc9e1b533c7c658aecd9b5aecf0d028d82c2a3d
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-10-16 20:15:53 +0200

    thinking ..

--- src/test/java/controller/collisions/CollisionsTest.java
+++ src/test/java/controller/collisions/CollisionsTest.java
@@ -1,5 +1,35 @@
 package controller.collisions;
 
+import controller.collisionEngine.CollisionEngine;
+import javafx.scene.layout.AnchorPane;
+import model.hud.HUDBonusImpl;
+import model.hud.HUDLifeImpl;
+import model.hud.HUDPointsImpl;
+import model.hud.IHUDBonus;
+import utilities.MagicEnumInt;
+import view.gameField.GameField;
+import view.gameField.GameFieldImpl;
+
 public class CollisionsTest {
 
+    private CollisionEngine engine;
+    private GameField gameField;
+    private HUDPointsImpl pointsHUD;
+    private HUDLifeImpl livesHUD;
+    private IHUDBonus bonusHUD;
+    private AnchorPane gamePane;
+
+    /**
+     * Constructor.
+     */
+    public CollisionsTest() {
+        this.gamePane = new AnchorPane();
+        this.gameField = new GameFieldImpl(MagicEnumInt.WIDTH.getValue(), 
+                                            MagicEnumInt.HEIGHT.getValue());
+        this.pointsHUD = new HUDPointsImpl();
+        this.livesHUD = new HUDLifeImpl(this.gamePane);
+        this.bonusHUD = new HUDBonusImpl(this.gameField);
+        this.engine = new CollisionEngine(this.gameField, this.pointsHUD, 
+                                            this.livesHUD, this.bonusHUD);
+    }
 }
