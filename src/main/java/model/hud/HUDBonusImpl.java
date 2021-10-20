1850181ee09601929b505635ea5bcc20b6a9a4b1
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-10-20 21:50:08 +0200

    sound added when bonus run off

--- src/main/java/model/hud/HUDBonusImpl.java
+++ src/main/java/model/hud/HUDBonusImpl.java
@@ -93,6 +93,7 @@ public class HUDBonusImpl implements IHUDBonus {
     @Override
     public final void hideBonus(final StatusEnum bonus) {
         try {
+            this.gameField.getSoundManager().playStatusFinish();
             this.gameField.getGameContainer().getChildren().remove(this.bonus[bonus.ordinal()]);
             this.statusTracker[bonus.ordinal()] = false;
         } catch (Exception e) {
