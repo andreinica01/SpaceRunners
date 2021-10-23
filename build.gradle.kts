dcc49e6b01b4957bbdf8e0920a23f2aaac6380b2
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2021-10-23 20:18:02 +0200

    Fixin access to rank

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     // JUnit API and testing engine
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
-    
 }
 
 tasks.withType<JavaCompile> {
